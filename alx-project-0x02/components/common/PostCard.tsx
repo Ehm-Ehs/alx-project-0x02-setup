beeed5ea5f42a035c0a496ce980ab5e9bd956132
Ehm-Ehs <salamimariam239@gmail.com>
2025-09-22 11:36:09 +0100

    updates

--- alx-project-0x02/components/common/PostCard.tsx
+++ alx-project-0x02/components/common/PostCard.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import { PostProps } from "@/interfaces";
 
-export default function PostCard({ userId, title, body }: PostProps) {
-  return (
-    <div className="border p-4 rounded mb-2">
-      <h3 className="font-bold">{title}</h3>
-      <p>{body}</p>
-      <span className="text-sm text-gray-500">User: {userId}</span>
-    </div>
-  );
-}
+const PostCard: React.FC<PostProps> = ({ userId, title, body }) => (
+  <div className="border p-4 rounded mb-2">
+    <h3 className="font-bold">{title}</h3>
+    <p>{body}</p>
+    <span className="text-sm text-gray-500">User: {userId}</span>
+  </div>
+);
+
+export default PostCard;
